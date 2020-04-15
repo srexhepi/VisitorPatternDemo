010c1aaae08a38d94c8be428fc43ea6d04a496b3
Shaban <shaban.rexhepi@hsr.ch>
2020-04-15 14:54:20 +0200

    Compilable Version of branch DoubleDispatchWithCompilationErrorDemo

--- DoubleDynamicDispatchDemo/Program.cs
+++ DoubleDynamicDispatchDemo/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 
+// Fixed the Compilation Failure of the branch DoubleDispatchWithCompilationErrorDemo
+
 namespace DoubleDynamicDispatchDemo {
     class Program {
         static void Main(string[] args) {
             Animal cat = new Cat();
             Animal dog = new Dog();
 
-            cat.makesound();
+            Console.WriteLine("Demonstration of Cat interacting with Dog");
             cat.makesound(dog);
+
+            Console.WriteLine("\nDemonstration of Dog interacting with Cat");
+            dog.makesound(cat);
+
+            Console.WriteLine("\nDemonstration of Dog interacting with Dog");
+            dog.makesound(dog);
+
+            Console.WriteLine("\nDemonstration of Cat interacting with Cat");
+            cat.makesound(cat);
         }
     }
 
@@ -15,6 +26,8 @@ namespace DoubleDynamicDispatchDemo {
         public abstract void makesound();
         public abstract void makesound(Cat cat);
         public abstract void makesound(Dog dog);
+
+        public abstract void makesound(Animal animal);
     }
 
     public class Cat : Animal {
@@ -27,6 +40,10 @@ namespace DoubleDynamicDispatchDemo {
 
         public override void makesound(Cat dog) {
             Console.WriteLine("Double Meow");
+        }
+
+        public override void makesound(Animal animal) {
+            Console.WriteLine("Cat reacts to Animal.");
         }
     }
 
@@ -41,6 +58,10 @@ namespace DoubleDynamicDispatchDemo {
 
         public override void makesound(Cat dog) {
             Console.WriteLine("Double Wuuf");
+        }
+
+        public override void makesound(Animal animal) {
+            Console.WriteLine("Dog reacts to Animal.");
         }
     }
 }
